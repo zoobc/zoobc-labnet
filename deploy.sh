d5da419f82747c303e6c693aab8b8a2bb1454e4b
Heri Sim <527101+heri16@users.noreply.github.com>
2019-10-25 16:59:48 +0800

    add appName param to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,10 @@ echo "[localhost] git pull..."
 git pull
 git submodule update --remote --init
 
-DOCKER_HOSTNAME="${1:-192.168.20.5}"
-DOCKER_PORT="${2:-2376}"
+APP_NAME="${1:-zbcDev}"
+DOCKER_HOSTNAME="${2:-192.168.20.5}"
+DOCKER_PORT="${3:-2376}"
 export NODE_EXTRA_CA_CERTS="$(pwd)/balena-ca.crt"
 export BALENARC_BALENA_URL="raspi.zoobc.org"
-echo "[${DOCKER_HOSTNAME}:${DOCKER_PORT}] balena deploy zbcDev --build"
-balena deploy zbcDev -h "${DOCKER_HOSTNAME}" -p "${DOCKER_PORT}" --ca "./.docker/ca.pem" --cert "./.docker/cert.pem" --key "./.docker/key.pem" --build
+echo "[${DOCKER_HOSTNAME}:${DOCKER_PORT}] balena deploy ${APP_NAME} --build"
+balena deploy "${APP_NAME}" -h "${DOCKER_HOSTNAME}" -p "${DOCKER_PORT}" --ca "./.docker/ca.pem" --cert "./.docker/cert.pem" --key "./.docker/key.pem" --build

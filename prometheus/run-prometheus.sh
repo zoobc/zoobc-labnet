3fd120f3c344f41d09d5e743f4640b7f3d51c818
Heri Sim <527101+heri16@users.noreply.github.com>
2019-10-29 17:10:12 +0800

    restart prometheus if exists

--- prometheus/run-prometheus.sh
+++ prometheus/run-prometheus.sh
@@ -6,4 +6,4 @@ if [ ! -f /var/lib/docker/volumes/prometheus-data/_data/prometheus.yml ]; then
   sudo cp ./prometheus.yml /var/lib/docker/volumes/prometheus-data/_data/
 fi
 sudo nano /var/lib/docker/volumes/prometheus-data/_data/prometheus.yml
-docker run -d --restart=always --name=prometheus -p 9090:9090 -v prometheus-data:/prometheus prom/prometheus --config.file=/prometheus/prometheus.yml
+docker run -d --restart=always --name=prometheus -p 9090:9090 -v prometheus-data:/prometheus prom/prometheus --config.file=/prometheus/prometheus.yml || docker restart prometheus
